75c58fed3a00aee0e00ab928ef12f4287bc876b3
RafaPaludo <rafael.ricardopp@gmail.com>
2024-02-22 16:34:32 -0300

    feat: add pinia store plugin

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@
 
 // Plugins
 import { registerPlugins } from '@/plugins'
+import { createPinia } from 'pinia'
 
 // Components
 import App from './App.vue'
@@ -15,9 +16,11 @@ import { createApp } from 'vue'
 import router from './router/index'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 registerPlugins(app)
 
 app
   .use(router)
+  .use(pinia)
   .mount('#app')

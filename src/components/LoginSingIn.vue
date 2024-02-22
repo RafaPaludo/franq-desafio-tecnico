2118c8cb7d6db38ec3ed4a29b9b3177f809ef9c8
RafaPaludo <rafael.ricardopp@gmail.com>
2024-02-22 15:49:51 -0300

    feat: separate login in and login up into differents components

---
+++ src/components/LoginSingIn.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-form v-model="valid" @submit.prevent="loginUser">
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="12"
+        >
+          <v-text-field
+            v-model="email"
+            label="E-mail"
+            :rules="rules"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="12"
+        >
+          <v-text-field
+            v-model="password"
+            type="password"
+            :rules="rules"
+            label="Senha"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-btn type="submit" block class="mt-2">Login</v-btn>
+  </v-form>
+
+  {{ successMessage }} {{ errorMessage }}
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import AuthenticationService from '@/services/AuthenticationService'
+
+const valid = ref(false)
+const password = ref('')
+const email = ref('')
+const successMessage = ref('')
+const errorMessage = ref('')
+const rules = []
+
+function loginUser () {
+  AuthenticationService.login({
+    email: email.value,
+    password: password.value
+  })
+  .then(response => {
+    successMessage.value = response?.data?.message
+  })
+  .catch(error => {
+    errorMessage.value = error?.response?.data?.message
+  })
+}
+</script>
